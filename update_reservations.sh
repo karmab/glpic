a448a3e1b823937abbeb4fbf69176155fda328a5
karmab <karimboumedhel@gmail.com>
2024-05-21 08:06:46 +0200

    limit reservations list

--- update_reservations.sh
+++ update_reservations.sh
@@ -1,10 +1,3 @@
 #!/usr/bin/env bash
 
-reservations="9833 9836 9837 9838 9839 11009 11011 11012 729 730 731 732 733 1363 8317 11010 11018 11019"
-
-now=$(date +%Y-%m-%d)
-one_month=$(date -d "$current_date + 1 month" +%Y-%m-%d)
-
-for reservation in $reservations ; do
-  glpic update reservation $reservation -P end="$one_month 00:00:00"
-done
+glpic update reservations 11009 11011 11012
